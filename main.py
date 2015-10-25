d0d185a9cf5d419afc4334f5670cac49c74f0a17
elParaguayo <antodudgeon@gmail.com>
2015-10-25 18:35:27 +0000

    Version bump

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from core.getplugins import getPlugins
 # Set the current working directory
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
-VERSION = "0.2"
+VERSION = "0.2.1"
 
 
 class InfoScreenApp(App):
